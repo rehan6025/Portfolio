f3d2501a76ab0e229c915f41b2d119d6b836fa89
rehan6025 <rehanthakur6025@gmail.com>
2025-10-11 16:39:50 +0530

    Hero section complete

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,75 @@
 import Image from "next/image";
+import LightRays from "@/components/LightRays";
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+    return (
+        <div className="relative w-full min-h-screen">
+            <LightRays
+                raysOrigin="top-center"
+                raysColor="#b89ef9"
+                raysSpeed={1.3}
+                lightSpread={1}
+                rayLength={1.5}
+                followMouse={true}
+                mouseInfluence={0.1}
+                noiseAmount={0.1}
+                distortion={0.05}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <main className="relative bg-transparent">
+                <div className="container px-4 sm:px-6 md:px-8 py-6 max-w-7xl mx-auto">
+                    <div className="w-full mx-auto bg-transparent max-w-4xl py-4 px-4 sm:py-10 sm:px-6 space-y-6">
+                        <div className="flex justify-between w-full items-center">
+                            <div className="flex gap-x-4">
+                                <div className="flex items-center">
+                                    <Image
+                                        src="/CrpMyImage.jpg"
+                                        alt="rehan-ahmed-image"
+                                        width={70}
+                                        height={70}
+                                    />
+                                </div>
+                                <div>
+                                    <h1 className="text-3xl font-semibold tracking-wide leading-normal">
+                                        Rehan Ahmed
+                                    </h1>
+                                    <div className="text-xl text-gray-400 tracking-normal">
+                                        <div>Software Engineer</div>
+                                        <div>Full Stack Developer</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <div className="hover:animate-pulse group relative">
+                                    <a
+                                        className="cursor-pointer tracking-wide text-gray-300 "
+                                        target="_blank"
+                                        href="https://drive.google.com/file/d/1oUXqlgN9MJeU6_vGArHE5awHylzXpL-M/view?usp=sharing"
+                                    >
+                                        <p className="group-hover:text-gray-100 transition-all ">
+                                            My Resume
+                                        </p>
+                                        <span
+                                            className=" pointer-events-none absolute left-0 bottom-0 h-[2px] w-full bg-gray-100
+           origin-center scale-x-0 transition-transform duration-300 ease-out
+           group-hover:scale-x-100"
+                                        ></span>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="w-full h-[1px] bg-border shrink-0"></div>
+                        <p className="text-white">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit. Curabitur in libero nec nulla blandit
+                            fermentum.
+                        </p>
+                        <p className="text-white">
+                            Another paragraph. Scroll will now scroll both
+                            content and rays together.
+                        </p>
+                    </div>
+                </div>
+            </main>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    );
 }
