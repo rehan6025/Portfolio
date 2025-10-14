5f64072c8ae7a56a9d9f774c43630047ae732f87
rehan6025 <rehanthakur6025@gmail.com>
2025-10-14 17:26:30 +0530

    projects section done , gif/images remain

--- components/ProjectsSection.tsx
+++ components/ProjectsSection.tsx
@@ -26,10 +26,24 @@ const ProjectsSection = () => {
     }, [opened]);
 
     const projects = [
-        { id: "project-1", title: "project1", desc: "this is project 1 " },
-        { id: "project-2", title: "project2", desc: "this is project 2 " },
-        { id: "project-3", title: "project3", desc: "this is project 3 " },
-        { id: "project-4", title: "project4", desc: "this is project 4 " },
+        {
+            id: "project-1",
+            title: "SketchRoom - Real-time Drawing Platform",
+            shortDesc:
+                "A real-time collaborative drawing platform with an infinite canvas, letting users create together seamlessly",
+            fullDesc:
+                "SketchRoom is a real-time collaborative drawing platform with an infinite canvas, enabling multiple users to draw, erase, zoom, and create together seamlessly in a clean interface.",
+            techs: [
+                "TypeScript",
+                "Next.js",
+                "Node.js",
+                "Postgres",
+                "WebSockets",
+            ],
+        },
+        { id: "project-2", title: "project2", shortDesc: "this is project 2 " },
+        { id: "project-3", title: "project3", shortDesc: "this is project 3 " },
+        { id: "project-4", title: "project4", shortDesc: "this is project 4 " },
     ];
 
     return (
@@ -50,8 +64,24 @@ const ProjectsSection = () => {
                             onClick={() => setOpened(project.id)}
                         >
                             <div className="h-[60%]">image</div>
-                            <div className="bg-zinc-900 p-4">section</div>
-                            <div className="p-4 bottom-0">Technology</div>
+                            <div className="bg-zinc-900 p-4 ">
+                                <h2 className="text-lg overflow-hidden whitespace-nowrap text-ellipsis">
+                                    {project.title}
+                                </h2>
+                                <p className="text-zinc-400 text-sm overflow-hidden text-ellipsis line-clamp-2 mb-4">
+                                    {project.shortDesc}
+                                </p>
+                                <div className="bottom-0 flex space-x-2 ">
+                                    {project.techs?.map((tech, index) => (
+                                        <div
+                                            className="text-xs py-1 px-2 text-black bg-zinc-500 rounded-full"
+                                            key={index}
+                                        >
+                                            {tech}
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
                         </div>
                     );
                 })}
